220d8844e27cb41a8f20839b17442689daaed95c
Stanislav Bebej <stanislav.bebej@gmail.com>
2024-02-15 14:36:25 +0100

    feat: build dev infrastructure with terraform

---
+++ terraform/gcloud.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.16.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project
+  region  = var.location
+}
