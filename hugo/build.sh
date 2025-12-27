fe60a3be638506a680fab497b6b6ababa8f9d862
Stanislav Bebej <stanislav.bebej@gmail.com>
2025-12-27 18:55:43 +0100

    .

--- hugo/build.sh
+++ hugo/build.sh
@@ -59,27 +59,8 @@ main() {
     git fetch --unshallow
   fi
 
-  # cd hugo
-
   # Create content
-  CONTENT_HOME="piesne"
-  SOURCE_DB="../db/jks.csv"
-
-  # Cleanup
-  rm -fr content/$CONTENT_HOME public/*
-
-  # Build
-  while IFS="," read -r piesen strofa text; do
-    text=${text//\"/}
-    CONTENT_FILE="${CONTENT_HOME}/${piesen}.md"
-
-    if [[ $strofa -eq 0 ]]; then
-      hugo new content $CONTENT_FILE -f
-      sed -i "s|title: .*|title: '${piesen}. ${text}'|" content/$CONTENT_FILE
-      continue
-    fi
-    echo "${strofa}. ${text}" >>content/$CONTENT_FILE
-  done <$SOURCE_DB
+  chmod a+x content.sh && ./content.sh
 
   # Build the site
   echo "Building the site..."
